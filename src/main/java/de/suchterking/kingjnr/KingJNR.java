01a5c38d39ad9b807adfff2b79e0cb2f653ddce2
SuchterKing <suchterking@gmail.com>
2023-04-23 17:30:15 +0200

    Update

--- src/main/java/de/suchterking/kingjnr/KingJNR.java
+++ src/main/java/de/suchterking/kingjnr/KingJNR.java
@@ -1,17 +1,39 @@
 package de.suchterking.kingjnr;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class KingJNR extends JavaPlugin {
 
+    List<String> jnrlist = new ArrayList<>();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        //config
+        loadConfig();
+        //JNR list
+        jnrlist.add((String) config().get("jnrList"));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        //JNR list
+        config().set("jnrList", jnrlist);
+    }
+
+    //config
+    public void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public static FileConfiguration config() {
+        FileConfiguration configuration = KingJNR.getPlugin(KingJNR.class).getConfig();
+        return configuration;
     }
 }
--- src/main/java/de/suchterking/kingjnr/commands/create.java
+++ src/main/java/de/suchterking/kingjnr/commands/create.java
@@ -1,4 +1,43 @@
 package de.suchterking.kingjnr.commands;
 
-public class create {
+import de.suchterking.kingjnr.KingJNR;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import de.suchterking.kingjnr.KingJNR.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class create implements CommandExecutor, TabCompleter {
+
+    FileConfiguration config = KingJNR.config();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
+        if (args.length == 2) {
+            if (args[0].equals("create")) {
+                config.set("jnr", args[1]);
+                config.set("jnr." + args[1] + ".name", args[2]);
+            }
+        }else {
+            player.sendMessage("§4[KINGJNR] Bitte nutze /kingjnr <create / delete> <name>");
+        }
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        List<String> list = new ArrayList<>();
+        list.add("create");
+        list.add("delete");
+        return list;
+    }
 }
